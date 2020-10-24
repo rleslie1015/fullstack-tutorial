fb2fb3a089223e083c02f1880688d3bbc0b5ac30
Leslie Rodriguez <rleslie1015@gmail.com>
2020-10-24 05:19:06 -0500

    completes mutation resolvers portion of tutorial

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -25,11 +25,61 @@ const typeDefs = gql`
         name: String
         missionPatch(size: PatchSize): String
     }
-      
-      enum PatchSize {
+    
+    type LaunchConnection { # add this below the Query type as an additional type.
+        cursor: String!
+        hasMore: Boolean!
+        launches: [Launch]!
+    }
+    enum PatchSize {
         SMALL
         LARGE
-      }
+    }
+
+    # Queries
+
+    # type Query {
+    #     launches: [Launch]!
+    #     launch(id: ID!): Launch
+    #     me: User
+    # }
+
+    type Query {
+
+        launches( # replace the current launches query with this one.
+          """
+          The number of results to show. Must be >= 1. Default = 20
+          """
+          pageSize: Int
+          """
+          If you add a cursor here, it will only return results _after_ this cursor
+          """
+          after: String
+        ): LaunchConnection!
+        launch(id: ID!): Launch
+        me: User
+    }
+
+    # Mutations
+
+    type Mutation {
+        bookTrips(launchIds: [ID]!): TripUpdateResponse!
+        cancelTrip(launchId: ID!): TripUpdateResponse!
+        login(email: String): String # login token
+    }
+      
+    # Mutation Response Types
+
+    type TripUpdateResponse {
+        success: Boolean!
+        message: String
+        launches: [Launch]
+    }
+      
+    type UserLoginResponse {
+        email: String
+        token: String
+    }
 `;
 
 module.exports = typeDefs;
